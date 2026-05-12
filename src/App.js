5d1a9552e34106895305a9560d0a6bb3038edb25
Romi <dev.mcdreamy@gmail.com>
2026-05-12 11:39:09 +0400

    fix: some changes related canvas env components

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import './App.css';
 import {Canvas} from '@react-three/fiber';
-import {OrbitControls} from '@react-three/drei';
 import Control from './components/Control';
 import Building from './components/Building';
+import CanvasEnv from './components/CanvasEnv';
 
 
 function App() {
@@ -10,24 +10,19 @@ function App() {
   return (
     <div className="App">
       <div style={{width: '100%',height: '100%',display: 'flex'}}>
-        <div style={{width: '80%',height: '100%',backgroundColor: 'cyan'}}>
-          <Canvas>
-            <group position={[0,-0.5,0]}>
-              <Building />
-            </group>
-            <ambientLight
-              intensity={3}
-            />
-            <OrbitControls
-              target={[0,10,0]}
-              enableDamping
-              dampingFactor={0.05}
-              minDistance={100}
-              maxDistance={700}
-              maxPolarAngle={Math.PI/2}
-              minAzimuthAngle={-Math.PI/3}
-              maxAzimuthAngle={Math.PI/3}
-            />
+        <div style={{width: '80%',height: '100%'}}>
+          <Canvas
+            id='canvas-container'
+            camera={{
+              fov: 45,
+              near: 0.1,
+              far: 1000,
+              aspect: window.innerWidth / window.innerHeight,
+              position: [0, 10, 200]
+            }}
+          >
+            <CanvasEnv />
+            <Building />
           </Canvas>
         </div>
         <div style={{width: '20%',height: '100%',backgroundColor: 'lightgreen',padding: '20px'}}>
---
+++ src/components/CanvasEnv/index.jsx
@@ -0,0 +1,23 @@
+import { Environment, OrbitControls } from "@react-three/drei"
+
+const CanvasEnv = () => {
+    return (
+        <>
+            <ambientLight intensity={3} />
+            <directionalLight position={[0, 100, 500]} intensity={3} />
+            <directionalLight position={[0, -100, -500]} intensity={2} />
+            <OrbitControls
+                target={[0,10,0]}
+                enableDamping
+                dampingFactor={0.05}
+                minDistance={100}
+                maxDistance={700}
+            />
+            <Environment preset="city" background backgroundBlurriness={1} backgroundIntensity={2} />
+
+            <axesHelper args={[500, 500, 500]} />
+        </>
+    )
+}
+
+export default CanvasEnv;
